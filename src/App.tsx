57b1c509163cf19e0c954c5d87c6dd8c36c4a01d
oluwatobicode <odetokuntreasure6@gmail.com>
2025-07-04 13:53:19 +0100

    added page visitors vercel extension

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import Start from "./pages/Start";
 import Game from "./pages/Game";
 import GameProvider from "./contexts/GameProvider";
+import { Analytics } from "@vercel/analytics/next";
 
 function App() {
   return (
     <GameProvider>
+      <Analytics />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Start />} />
